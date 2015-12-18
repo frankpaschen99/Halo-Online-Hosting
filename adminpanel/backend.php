e110b0a6e13408f68f579c36a3a3d5c1c013166d
Frank Paschen <frankpaschen99@gmail.com>
2015-12-17 20:57:58 -0500

    update admin panel

--- adminpanel/backend.php
+++ adminpanel/backend.php
@@ -5,7 +5,7 @@ $ssh = new SSHWrapper("localhost", "22", "19:ce:ad:e5:9a:3f:30:b1:85:31:95:68:bb
 						"frank@frank-VirtualBox", "/home/.ssh/id_rsa.pub", "/home/.ssh/id_rsa");
 
 function mkdir_new_user($username) {
-	// Create and enter new directory
+	// Create and enter new directory on the vm
 	$ssh->exec("mkdir /home/$username && cd /home/$username");
 }
 ?>
